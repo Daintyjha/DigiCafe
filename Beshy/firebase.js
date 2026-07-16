caf277171787d834511796deb08acfa55ee0340e
Daintyjha <81165159+Daintyjha@users.noreply.github.com>
2026-07-16 15:05:43 +0200

    Update firebase.js

--- Beshy/firebase.js
+++ Beshy/firebase.js
@@ -1,57 +1,31 @@
+// Beshy Firebase Connection
+
 import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import {
-    getFirestore,
-    collection,
-    addDoc,
-    getDocs,
-    query,
-    where
-}
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-
-// Firebase configuration
-// Replace these with your Firebase details
 
 const firebaseConfig = {
 
-    apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBLI3WImMtnSWD5KtaHAu3s3iFnZNkkL74",
 
-    authDomain: "YOUR_AUTH_DOMAIN",
+  authDomain: "digicafe-beshy.firebaseapp.com",
 
-    projectId: "YOUR_PROJECT_ID",
+  projectId: "digicafe-beshy",
 
-    storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "digicafe-beshy.firebasestorage.app",
 
-    messagingSenderId: "YOUR_MESSAGING_ID",
+  messagingSenderId: "359684807397",
 
-    appId: "YOUR_APP_ID"
-
-};
-
-
-const app =
-initializeApp(firebaseConfig);
-
-
-const db =
-getFirestore(app);
-
-
-export {
-
-    db,
-
-    collection,
-
-    addDoc,
-
-    getDocs,
-
-    query,
-
-    where
+  appId: "1:359684807397:web:ebc66e014e04694361120c"
 
 };
+
+
+const app = initializeApp(firebaseConfig);
+
+
+// This allows Beshy to use Firestore
+export const db = getFirestore(app);
