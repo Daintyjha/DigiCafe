36faaada2ed6d908fa077085a23b6889034d10cf
Daintyjha <81165159+Daintyjha@users.noreply.github.com>
2026-07-17 14:24:58 +0200

    Initialize Firebase app and Firestore connection

---
+++ firebase.js
@@ -0,0 +1,37 @@
+// Beshy Firebase Connection
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBLI3WImMtnSWD5KtaHAu3s3iFnZNkkL74",
+
+  authDomain: "digicafe-beshy.firebaseapp.com",
+
+  projectId: "digicafe-beshy",
+
+  storageBucket: "digicafe-beshy.firebasestorage.app",
+
+  messagingSenderId: "359684807397",
+
+  appId: "1:359684807397:web:ebc66e014e04694361120c"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+// This allows Beshy to use Firestore
+export const db = getFirestore(app);
+
+
+// This allows Beshy to use Firebase Functions
+export { app };
+const app = initializeApp(firebaseConfig);
+
+
+// This allows Beshy to use Firestore
+export const db = getFirestore(app);
