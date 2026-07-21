3f29f68a0c53e2f48ad3af511551979516f8eb3b
Daintyjha <81165159+Daintyjha@users.noreply.github.com>
2026-07-21 11:23:32 +0200

    Clean up firebase.js and initialize Firestore
    Removed unused SDK imports and added Firestore initialization.

--- firebase.js
+++ firebase.js
@@ -8,16 +8,6 @@ from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
 
 const firebaseConfig = {
 
@@ -31,7 +21,8 @@ const firebaseConfig = {
 
   messagingSenderId: "359684807397",
 
-  appId: "1:359684807397:web:ebc66e014e04694361120c"
+ appId: "1:359684807397:web:ebc66e014e04694361120c"
+
 
 };
 
@@ -39,3 +30,12 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firestore
+
+const db = getFirestore(app);
+
+
+console.log("🔥 Firebase connected:", app.name);
+
